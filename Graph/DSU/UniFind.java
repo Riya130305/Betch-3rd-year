0fba2cfa3b9b53f1665de62cfaae35b4fd85db1b
Riya Khandelwal <riyakhandelwal4776@gmail.com>
2025-09-16 14:37:35 +0530

    unionfind

---
+++ Graph/DSU/UniFind.java
@@ -0,0 +1,64 @@
+import java.util.*;
+class UnionFind
+{
+    int[] parent;
+    int[] rank;
+    UnionFind(int V)
+    {
+        parent=new int[V+1];
+        rank=new int[V+1];
+
+        for(int i=0;i<=V;i++)
+        {
+            parent[i]=i;
+        }
+    }
+
+    int find(int node)
+    {
+        if(parent[node]==node)
+        {
+            return node;
+        }
+        // RECURSIVLEY CALL KARNA HAI PARENT
+        return find(parent[node]);
+    }
+
+    boolean union(int u,int v)
+    {
+        int pu=find(u);
+        int pv=find(v);
+        // if parent are same so we will not do any union(ki dono already es ho leader ke andder hai)
+        if(pv==pu)
+        {
+            return false;
+        }
+        // phle leader tk jaao and then rank ke according attach kaaro(jada rank ke neeche kam rank wala add ho jaaega) 
+       else if(rank[pu]>rank[pv])
+        {
+            parent[pv]=pu;
+        }
+        else//if rank is same toh kisi ko bhi kisi ke sath add krdo
+        {
+            parent[pv]=pu;
+            rank[pu]++;
+        }
+        return true;
+
+    }
+}
+public class UniFind {
+    public static void main(String[] args) {
+        UnionFind uf= new UnionFind(3);
+        uf.union(1,2);
+        uf.union(1,3);
+        if(uf.union(2,3))
+        {
+            System.out.println("possible");
+        }
+        else
+        {
+            System.out.println("Not possible");
+        }
+    }
+}
