2b9b93894a7bf43c31ef256e5986111a07bd297a
Riya Khandelwal <riyakhandelwal4776@gmail.com>
2025-10-22 15:16:20 +0530

    height of tree

---
+++ Binary_Tree/QuestionSet1/Height__BT.java
@@ -0,0 +1,60 @@
+package QuestionSet1;
+
+import java.util.Scanner;
+
+public class Height__BT {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter a root node");
+        int rootVal=sc.nextInt();
+        Node root= new Node(rootVal);
+        buildTree(root,sc);
+        int ans = height(root);
+        System.out.println("ans-->"+ans);
+    }
+    public static void buildTree(Node root, Scanner  sc)
+    {
+        // left child
+        System.out.println("want to enter left child");
+        boolean hasLeft=sc.nextBoolean();
+        if(hasLeft)
+        {
+            System.out.println("left");
+            int left =sc.nextInt();
+            root.left=new Node(left);
+            buildTree(root, sc);
+        }
+          // left child
+        System.out.println("want to enter right child");
+        boolean hasRight=sc.nextBoolean();
+        if(hasRight)
+        {
+            System.out.println("right");
+            int right =sc.nextInt();
+            root.right=new Node(right);
+            buildTree(root, sc);
+        }
+    }
+    public static  int height(Node root) {
+        // code here
+        if(root==null)
+        return 0;
+        if(root.left==null && root.right==null)
+        return 0;
+        return 1 + Math.max(height(root.left),height(root.right));
+        
+    }
+}
+class Node {
+    int data;
+    Node right;
+    Node left;
+    Node(int data)
+    {
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+}
+
+
