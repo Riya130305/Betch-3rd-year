bb6a57c2aff4594ebc30e46622a209772b636879
Riya Khandelwal <riyakhandelwal4776@gmail.com>
2026-01-10 12:35:15 +0530

    change the folder name

---
+++ Graph/BFS_Question/Rotten_oranges.java
@@ -0,0 +1,70 @@
+package BFS_Question;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Rotten_oranges {
+    public static void main(String[] args) {
+        int[][] grid={
+            {2,1,1},
+            {1,1,0},
+            {0,1,1}
+        };
+        int res=orangesRotting(grid);
+        System.out.println(res); // Expected: 4
+    }
+     public static int orangesRotting(int[][] grid) {
+        //BFS
+        
+        int n=grid.length;
+        int m=grid[0].length;
+        int freshOrg=0;
+        //int[][] visited= new int[n][m];
+        Queue<int[]> q= new LinkedList<>();
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                //phle sare rotten oranges ek sath quue mai add kr diye haii 
+                if(grid[i][j]==2)
+                {
+                    q.add(new int[]{i,j,0});
+                }
+                else if(grid[i][j]==1)
+                {
+                    freshOrg++;
+                }
+            }
+        }
+        return bfs(grid,n,m,freshOrg,q);
+    }
+    public static int bfs(int[][] grid,int n, int m,int freshOrg,Queue<int[]>q)
+    {
+        int[] row={0,-1,0,1};
+        int[] col={-1,0,1,0};
+        int maxTime=0;
+        
+        while(!q.isEmpty())
+        {
+            int[] val=q.poll();
+            int u=val[0];
+            int v=val[1];
+            int wt=val[2];
+            maxTime=Math.max(maxTime,wt);
+            for(int k=0;k<4;k++)
+            {
+                int nrow=u+row[k];
+                int ncol=v+col[k];
+
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1)
+                {
+                    freshOrg--;
+                    grid[nrow][ncol]=2;
+                    q.add(new int[]{nrow,ncol,wt+1});
+                }
+            }
+        }
+        return freshOrg==0 ? maxTime : -1;
+    }
+}
